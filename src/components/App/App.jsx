184e1febf46118d9c4b4be6acd98a19a95423290
IiIymik <sambotemp@gmail.com>
2021-07-14 16:42:36 +0300

    add Hooks for App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,60 +1,67 @@
-import React, { Component } from 'react';
+import {useState} from 'react';
 import PropTypes from 'prop-types';
 import Notification from '../Notification/Notification';
 import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
 import Statistics from '../Statistics/Statistics';
 import { Container } from './App.styled.js';
 
+// Need add to Statistics total={},and positivePercentage={positiveFeed} and do some Hooks 
 
+function App() {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+    const [visible, setVisible] = useState(false);
+    const feedbackBtnEl = Object.keys({ good, neutral, bad });
 
-export default class App extends Component {
-      static defaultProps = {
-        initialValue: 0,
-    }
-    static propTypes = {
-        good: PropTypes.number,
-        neutral: PropTypes.number,
-        bad: PropTypes.number,
-        visible:PropTypes.bool,
-    }
-    state = {
-        good: this.props.initialValue,
-        neutral: this.props.initialValue,
-        bad: this.props.initialValue,
-        visible: false,
-    }
-      
-    incrementStatistics = (e) => {
+    const incrementStatistics = (e) => {
         const { name } = e.target;
-        this.setState((prevState) => ({
-            [name]: prevState[name] + 1,
-        }));
-        this.changeVisible();
+        switch (name) {
+            case 'good':
+                setGood(state => state + 1)
+                break;
+            case 'neutral':
+                setNeutral(state => state + 1)
+                break;
+            case 'bad':
+                setBad(state => state + 1)
+                break;
+            default: console.log("not worked");
+        }
+       setVisible(true)
     }
 
-    changeVisible = () => {
-        this.setState({visible: true})
-    }
-
-    countTotalFeedback = () => {
-        const { good, neutral, bad} = this.state;
-        return good + bad + neutral;
-    }
-    countPositiveFeedbackPercentage = (totalSum) => {
-        const { good } = this.state;
-        return Math.round(good / totalSum * 100);
-    }
-  render() {
-        const { good, neutral, bad, visible} = this.state;
-        const totalSum = this.countTotalFeedback();
-        const positiveFeed = this.countPositiveFeedbackPercentage(totalSum);
-    
-        return (
-            <Container>
-                <FeedbackOptions onLeaveFeedback={this.incrementStatistics} options={Object.keys({good,neutral,bad})} />
+    return (
+        <Container>
+                <FeedbackOptions onLeaveFeedback={incrementStatistics} options={feedbackBtnEl} />
                 {!visible && <Notification message="No feedback given"/> }
-                { visible && <Statistics good={good} neutral={neutral} bad={bad} total={totalSum} positivePercentage={positiveFeed}/>}
+                { visible && <Statistics good={good} neutral={neutral} bad={bad}  />}
             </Container>
-        )
-  }
+    )
 }
+
+export default App
+
+
+
+
+
+
+//     countTotalFeedback = () => {
+//         const { good, neutral, bad} = this.state;
+//         return good + bad + neutral;
+//     }
+//     countPositiveFeedbackPercentage = (totalSum) => {
+//         const { good } = this.state;
+//         return Math.round(good / totalSum * 100);
+//     }
+//   render() {
+//         const { good, neutral, bad, visible} = this.state;
+//         const totalSum = this.countTotalFeedback();
+//         const positiveFeed = this.countPositiveFeedbackPercentage(totalSum);
+    
+//         return (
+//             <div></div>
+//         )
+//   }
+// }
